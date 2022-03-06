d8f5d35489b386e0769a9d74d86677ede6d19fd9
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-03-06 17:58:00 -0300

    moduled styles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,8 @@
-import style from '../styles/home.module.css';
+import style from '../styles/home.module.css'; // works only in the file thats are imported and used
 
 export default function App() {
   return (
-    <div style={style.pedro}>
+    <div className={style.pedro}>
       Hello Word
     </div>
   )
